50d210100e0ee0db4b54e87d7235cf86e2e19835
vaishnav80 <vaishnavpuzhakkal3@gmail.com>
2025-01-28 20:20:35 +0530

    ci/cd test 7

--- Backend/Root/order/views.py
+++ Backend/Root/order/views.py
@@ -14,7 +14,7 @@ class Orders(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self,request):
-        print('priced')
+        print('pricedd')
         data = request.data.copy()
 
         print('data',data)
